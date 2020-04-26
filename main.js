c31ceaf483f564911f10f1dbed49cf66098799b5
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-04-26 17:08:10 +0200

    fixed bugs with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -55,6 +55,9 @@ function writeToDocument(url) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(
+            /,/g,
+            ""
+        );
     });
 }
